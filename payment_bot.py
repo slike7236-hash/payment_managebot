84c3e037b42f8bcfb1cd9c7ce0b6bc278b30fa2c
slike7236-hash <slike7236@gmail.com>
2026-09-13 12:31:27 +0500

    Integrate Flask server for bot persistence
    Added a Flask web server to keep the bot alive.

--- payment_bot.py
+++ payment_bot.py
@@ -1,4 +1,25 @@
 import os
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.daemon = True
+    t.start()
+
+# Bot kodingiz boshlanishidan oldin chaqiring:
+keep_alive()
+import os
 import logging
 from dotenv import load_dotenv
 
